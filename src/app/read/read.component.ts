2ce58a854e328fd1d0a2116006cfa5ca40468c48
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-17 10:26:29 +0100

    bm: partial console beta imp

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IScripture {
+  book: string;
+  chapter: number;
+  verse: number;
+}
+
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.css']
 })
 export class ReadComponent implements OnInit {
+  private _scripture: IScripture =  {
+    book: 'genesis',
+    chapter: 1,
+    verse: 1
+  };
+  public scripture: IScripture = this._scripture;
+  public bookList: any = [
+    'genesis',
+    'exodus'
+  ];
 
   constructor() { }
 
