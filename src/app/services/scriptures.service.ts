06a6a1cc5cc4eaa75ec135f10c120f2bb23d779c
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-18 19:50:20 +0100

    bm: beta imp for whole book

--- src/app/services/scriptures.service.ts
+++ src/app/services/scriptures.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { distinct } from 'rxjs/operators';
+
+import { ResourceHandlerService } from './resource-handler.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ScripturesService {
+  public wholeBook: Observable<any>;
+  constructor(
+    private _resources: ResourceHandlerService
+  ) { }
 
-  constructor() { }
+  public getBookList(): Observable<any> {
+    return new Observable((obs) => {
+      this._resources.fetchResource()
+      .subscribe(
+        (data) => {
+          data.request
+            .forEach(
+              (cur: any) => obs.next(cur.bookTitle)
+            );
+        },
+        (error) => {
+          obs.error(error);
+        }
+      );
+    })
+    .pipe(
+      distinct()
+    );
+  }
+
+  public getBible(): void {
+    this._resources.fetchResource()
+      .subscribe(
+        (data) => {
+          this.wholeBook = of(...data);
+        },
+        (error) => {
+          console.log({error});
+        }
+      );
+  }
 }
