33c7dbb30f614c9f16bd32b4ab11afcaae065b3a
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-19 19:13:04 +0100

    bm: player methods

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { SpeechSynthesisService } from './speech-synthesis.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(
+    private _speechSynth: SpeechSynthesisService
+  ) { }
+
+  public play(content: string): void {
+    this._speechSynth.play(content);
+  }
+
+  public pause(): void {
+    this._speechSynth.pause();
+  }
+
+  public stop(): void {
+    this._speechSynth.stop();
+  }
+
+  public get isPending():boolean {
+    return this._speechSynth.isPending();
+  }
 }
