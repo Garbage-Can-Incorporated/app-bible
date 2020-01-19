00bdcc27b040007dcae62935c9a74d02a28407f8
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-19 19:17:45 +0100

    bm: beta methods imp

--- src/app/services/speech-synthesis.service.ts
+++ src/app/services/speech-synthesis.service.ts
@@ -4,6 +4,49 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SpeechSynthesisService {
+  private synth = window.speechSynthesis;
+  private volume: number;
 
-  constructor() { }
+  constructor() {
+    this.setVolume();
+  }
+
+  public play(content: string): void {
+    if (this.synth.paused) {
+      this.synth.resume();
+      return;
+    }
+
+    const utterance = this.speechSynthUtterance(content);
+    utterance.volume = this.volume;
+    utterance.lang = `en-NG`;
+
+    this.synth.speak(utterance);
+  }
+
+  public stop(): void {
+    this.synth.cancel();
+  }
+
+  public pause(): void {
+    if (!this.synth.paused) {
+      this.synth.pause();
+    }
+  }
+
+  public isPending(): boolean {
+    return this.synth.pending;
+  }
+
+  private speechSynthUtterance(content) {
+    return new SpeechSynthesisUtterance(content);
+  }
+
+  public setVolume(level: number = 0.5): void {
+    this.volume = level;
+  }
+
+  public getVolume(): number {
+    return this.volume;
+  }
 }
