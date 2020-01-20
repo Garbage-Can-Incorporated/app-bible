7dd5caaac227280a2ede83aaa1b5b23115921dfb
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-20 01:27:16 +0100

    bm: speech meth beta methods imp

--- src/app/services/speech-synthesis.service.ts
+++ src/app/services/speech-synthesis.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SpeechSynthesisService {
   private synth = window.speechSynthesis;
   private volume: number;
+  private pitch = <number>1;
+  private rate = <number>1;
+  private voicesList: any[];
 
-  constructor() {
-    this.setVolume();
-  }
+  constructor() { }
 
-  public play(content: string): void {
-    if (this.synth.paused) {
-      this.synth.resume();
-      return;
-    }
+  public play (content: string): Observable<any> {
+    return new Observable(
+      (obs) => {
+        if (this.synth.speaking === true) {
+          console.log('was speaking!');
+          this.synth.resume();
+          obs.next({ status: true, msg: 'Resumed speaking' });
+          return;
+        }
 
-    const utterance = this.speechSynthUtterance(content);
-    utterance.volume = this.volume;
-    utterance.lang = `en-NG`;
+        const utterance = this.speechSynthUtterance(content);
+        utterance.volume = this.volume;
+        utterance.lang = `en-NG`;
+        utterance.pitch = this.pitch;
+        utterance.rate = this.rate;
 
-    this.synth.speak(utterance);
+        if (this.voicesList.length !== 0) {
+          utterance.voice = this.voicesList[
+            this.preferedVoice()
+          ];
+
+          this.synth.speak(utterance);
+          obs.next({status: true, msg: 'Success!'});
+
+          utterance.onend = () => {
+            obs.complete();
+          };
+        } else {
+          console.log('voice models depends on the internet');
+          obs.error({
+            status: false,
+            msg: 'voice models depends on the internet'
+          });
+        }
+      }
+    );
   }
 
   public stop(): void {
@@ -29,8 +57,10 @@ export class SpeechSynthesisService {
   }
 
   public pause(): void {
-    if (!this.synth.paused) {
+    if (this.synth.paused === false) {
       this.synth.pause();
+      console.log('original pause');
+      return;
     }
   }
 
@@ -48,5 +78,29 @@ export class SpeechSynthesisService {
 
   public getVolume(): number {
     return this.volume;
+  }
+
+  private getVoices(): any[] {
+    this.voicesList = speechSynthesis.getVoices();
+    return this.voicesList;
+  }
+
+  public isVoiceAvailable(): any[] {
+    this.getVoices();
+    return this.voicesList;
+  }
+
+  public __init__ (): void {
+    this.setVolume();
+    this.getVoices();
+  }
+
+  public preferedVoice(): number {
+    return this.voicesList.findIndex(
+      (cur) => cur.lang === 'en-US' ||
+                cur.voiceURI === 'Google US English' ||
+                cur.name === 'Google US English' ||
+                (cur.name.includes('English') && cur.lang === 'en-US')
+    );
   }
 }
