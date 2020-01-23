b6f4ab4e2f1fd667e4a3605b4a2157862dc7b993
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-23 09:24:31 +0100

    bm: el scroll position visibility directive

---
+++ src/app/directives/action-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ActionScrollDirective } from './action-scroll.directive';
+
+describe('ActionScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ActionScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/action-scroll.directive.ts
@@ -0,0 +1,42 @@
+import { Directive, Input, DoCheck, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appActionScroll]'
+})
+export class ActionScrollDirective implements DoCheck, OnInit {
+  @Input() reveal: string;
+  @Input() _class: string;
+
+  private pos: number;
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    this.pos = this.getCurrentPos();
+    console.log({pos: this.pos});
+  }
+
+  ngDoCheck(): void {
+    console.log(
+      {cur: this.getCurrentPos(), pos: this.pos}
+    );
+    if (
+      this.reveal !== undefined &&
+      this._class !== undefined
+    ) {
+      this.toggleClassList(this.el.nativeElement);
+    }
+  }
+
+  private toggleClassList (icon: Element): void {
+    if (this.getCurrentPos() < this.pos) {
+      icon.classList.remove(this._class);
+    } else {
+      icon.classList.add(this._class);
+    }
+  }
+
+  private getCurrentPos(): number {
+    return this.el.nativeElement.getBoundingClientRect().top;
+  }
+}
