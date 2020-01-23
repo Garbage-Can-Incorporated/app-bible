9a67c62b1d12e2e2dc8b1d896d57aff8994cc400
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-23 10:39:33 +0100

    bm: beta imp

--- src/app/services/search-scriptures.service.ts
+++ src/app/services/search-scriptures.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
 
+import Fuse from 'fuse.js';
+import { ScripturesService } from './scriptures.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SearchScripturesService {
+  private options: any = {
+    caseSensitive: true,
+    shouldSort: true,
+    tokenize: true,
+    matchAllTokens: true,
+    findAllMatches: true,
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 100,
+    minMatchCharLength: 1,
+    keys: ['verse']
+  };
 
-  constructor() { }
+  public data: Array<any>;
+  public fuse: any;
+
+  constructor(
+    private _scripture: ScripturesService
+  ) {}
+
+  public __init__(cb): void {
+    this._scripture
+      ._getBible(
+        (data: Array<any>) => {
+          this.data = data;
+          this.fuse = new Fuse(this.data, this.options);
+          cb();
+        }
+      );
+  }
+
+  public search(string: string): any {
+    return this.fuse.search(string);
+  }
+
+  public changeOptions(): any {
+    return this.options;
+  }
 }
