df3d27acaad1489927eb9a06d47c935ac8116d03
Israel Oluwole <olaleyeisrael@gmail.com>
2020-02-11 22:58:10 +0100

    bm: button module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { HttpClientModule } from "@angular/common/http";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatButtonModule } from "@angular/material/button";
 
-import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { TypeaheadModule } from "ngx-bootstrap/typeahead";
 
-import { AppComponent } from './app.component';
-import { ReadComponent } from './read/read.component';
-import { SearchPaneComponent } from './search-pane/search-pane.component';
+import { AppComponent } from "./app.component";
+import { ReadComponent } from "./read/read.component";
+import { SearchPaneComponent } from "./search-pane/search-pane.component";
 
-import { ScripturesService } from './services/scriptures.service';
-import { ResourceHandlerService } from './services/resource-handler.service';
-import { PlayerService } from './services/player.service';
-import { SpeechSynthesisService } from './services/speech-synthesis.service';
+import { ScripturesService } from "./services/scriptures.service";
+import { ResourceHandlerService } from "./services/resource-handler.service";
+import { PlayerService } from "./services/player.service";
+import { SpeechSynthesisService } from "./services/speech-synthesis.service";
 
-import { ScrollViewDirective } from './directives/scroll-view.directive';
-import { ActionScrollDirective } from './directives/action-scroll.directive';
-import { ScriptureNavigatorDirective } from './directives/scripture-navigator.directive';
+import { ScrollViewDirective } from "./directives/scroll-view.directive";
+import { ActionScrollDirective } from "./directives/action-scroll.directive";
+import { ScriptureNavigatorDirective } from "./directives/scripture-navigator.directive";
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { ScriptureNavigatorDirective } from './directives/scripture-navigator.di
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatButtonModule,
     TypeaheadModule.forRoot(),
     HttpClientModule,
     MatProgressBarModule
@@ -54,4 +56,4 @@ import { ScriptureNavigatorDirective } from './directives/scripture-navigator.di
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
