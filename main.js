3045638f22b8038711ff2fb0ac1b8e7cc2ae62a1
Israel Oluwole <olaleyeisrael@gmail.com>
2020-02-24 14:51:42 +0100

    bm: electron temp. imp.

---
+++ main.js
@@ -0,0 +1,67 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+const createWindow = () => {
+  const {screen} = require('electron');
+
+  win = new BrowserWindow({
+    frame: false,
+    center: true,
+    moveable: true,
+    resizable: true,
+    autoHideMenuBar: true,
+    title: 'goshen',
+    titleBarStyle:
+      process.platform !== 'darwin' ?
+        'hiddenInset' :
+        'customButtonsOnHover',
+    webPreferences: {
+      experimentalFeatures: true,
+      nodeIntegration: true,
+    },
+  });
+
+  const {
+    // width: displayWidth,
+    height: displayHeight,
+  } = screen
+      .getPrimaryDisplay()
+      .size;
+
+  win.setMinimumSize(768, displayHeight - 40);
+  win.setMaximumSize(992, displayHeight);
+
+  win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, `/dist/app-bible/index.html`),
+        protocol: 'file:',
+        slashes: true,
+      })
+  );
+
+  // The following is optional and will open the DevTools:
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
