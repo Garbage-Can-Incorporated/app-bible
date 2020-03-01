0027f211139564fe74af1abd52f07a86579333ba
Israel Oluwole <olaleyeisrael@gmail.com>
2020-03-01 16:03:34 +0100

    bm: renderer bw service beta imp

---
+++ src/app/services/renderer-bw.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+
+import { TwitterShare } from '../interfaces/share';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RendererBwService {
+  private bw: Electron.BrowserWindow;
+
+  constructor(
+    private _electron?: ElectronService
+  ) { }
+
+  public createWindow(
+    url: string,
+    data: TwitterShare,
+    windowConfig: Electron.BrowserWindowConstructorOptions = {
+      center: true,
+      width: 575,
+      movable: true,
+      modal: true,
+      autoHideMenuBar: true,
+    }
+  ): Promise<void> {
+    this.bw = new this._electron.remote.BrowserWindow(windowConfig);
+    console.log('I was clicked');
+
+    return this.bw
+      .loadURL(
+        url
+          .concat(`text=${ data.text }`)
+          .concat(`&via=${ data.via }`)
+          .concat(`&hashtags=${data.hashtags}`)
+      );
+  }
+}
