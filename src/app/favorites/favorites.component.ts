282f0e311a51827575142d21845147c71d1e4eac
Israel Oluwole <olaleyeisrael@gmail.com>
2020-03-06 18:07:31 +0100

    items rendering

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges, Renderer2 } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { DbIpcService } from '../services/db-ipc.service';
+import { ElectronService } from 'ngx-electron';
+import { ScripturesService } from '../services/scriptures.service';
+
+import { IScriptures } from '../interfaces/i-scriptures';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent implements OnInit {
+export class FavoritesComponent implements OnInit, OnChanges, OnDestroy {
+  public items: Array<string> = [];
+  private dbStatus: any;
+  private tbStatus: any;
 
-  constructor() { }
+  constructor(
+    private _dbIPC: DbIpcService,
+    private _scriptureService: ScripturesService,
+    private renderer: Renderer2,
+    private _electron?: ElectronService
+  ) { }
 
   ngOnInit() {
+    this._dbIPC
+      .isDBCreated('favorites');
+    this.checkStatus();
+    this.retrieveFavList();
   }
 
+  ngOnChanges() {
+    if (this._electron.isElectronApp) {
+      this._dbIPC
+        .isConnected('favorites')
+        .subscribe(
+          (data: { dbInit: boolean, tableInit: boolean }) => {
+            this.dbStatus = data.dbInit;
+            this.tbStatus = data.tableInit;
+          }
+      );
+
+      this.retrieveFavList();
+    }
+  }
+
+  ngOnDestroy() {
+    // this._dbIPC.subjects.isConnectedSubject.unsubscribe();
+  }
+
+  private retrieveFavList(): void {
+    if (this._electron.isElectronApp) {
+      this._electron.ipcRenderer.send('list-fav-items');
+
+      this._electron.ipcRenderer
+        .on(
+          'favorite-items-listed',
+          (e, data: Array<IScriptures>) => {
+            data
+              .forEach((cur: IScriptures) => {
+                this
+                  .getPassage(cur.book, cur.chapter)
+                  .subscribe(
+                    (_data) => {
+                      this.items.push(_data[cur.verse]);
+                    },
+                    (error) => console.log({ error }),
+                    () => console.log('complete!')
+                  );
+              });
+          });
+    }
+  }
+
+  private getPassage(book: string, chapter: number): Observable<any> {
+    return this._scriptureService.getPassage(book, chapter);
+  }
+
+  private checkStatus(): void {
+    this._dbIPC.isConnected('favorites')
+    .subscribe(
+      (data) => {
+        console.log({ data });
+        this.dbStatus = data.dbInit;
+        this.tbStatus = data.tableInit;
+      },
+      (error) => console.log(`[Error] db connection error`)
+    );
+  }
 }
