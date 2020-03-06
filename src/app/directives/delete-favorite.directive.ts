3cb6003c754c51dc102f99a45949a0093c8f4575
Israel Oluwole <olaleyeisrael@gmail.com>
2020-03-06 20:03:19 +0100

    delete favorite at more beta... sleepy head

---
+++ src/app/directives/delete-favorite.directive.ts
@@ -0,0 +1,46 @@
+import { Directive, HostListener, Input } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+
+import { IScriptures } from '../interfaces/i-scriptures';
+
+@Directive({
+  selector: '[appDeleteFavorite]'
+})
+export class DeleteFavoriteDirective {
+  @Input() public data: IScriptures;
+
+  constructor(
+    private _electron: ElectronService
+  ) { }
+
+  @HostListener('click', [ '$event' ])
+  onClick(e) {
+    console.log({ e });
+    this.removeItem(e.target);
+  }
+
+  private removeItem(el: any): void {
+    this._electron.ipcRenderer
+      .send(
+        'remove-fav-item',
+        {
+          scripture: {
+            book: this.data.book,
+            chapter: this.data.chapter,
+            verse: this.data.verse,
+          }
+        }
+      );
+
+    this._electron.ipcRenderer
+      .on(
+        'fav-item-removal-status',
+        (e, data) => {
+          console.log({ data });
+
+          if (data.status) {
+            el.remove();
+          }
+    });
+  }
+}
