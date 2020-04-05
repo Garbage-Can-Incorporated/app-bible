05006edd9b430368f206c33db60d26d9e157cad1
Israel Oluwole <olaleyeisrael@gmail.com>
2020-04-05 03:26:32 +0100

    label dialog beta

--- src/app/label/label.component.ts
+++ src/app/label/label.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-label',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _dialogRef: MatDialogRef<LabelComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+  ) { }
 
   ngOnInit() {
+    this._dialogRef.addPanelClass('__app-label__background--black');
   }
 
+  public onSubmit(): void {
+    this._dialogRef.close(this.data);
+  }
 }
