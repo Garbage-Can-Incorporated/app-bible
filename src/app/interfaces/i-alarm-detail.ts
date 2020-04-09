1e4954c76b3a5c4df337c8c766cc37f5f0a682df
Israel Oluwole <olaleyeisrael@gmail.com>
2020-04-09 07:54:43 +0100

    new prop

--- src/app/interfaces/i-alarm-detail.ts
+++ src/app/interfaces/i-alarm-detail.ts
@@ -3,6 +3,7 @@ export interface IAlarmDetail {
   time: number;
   repeat: boolean; // repeat determines if day would be populated
   days?: Array<number>; // array of numbers 0 -> 6
+  _days?: Array<number>; // alternate days
   label:  string; // content of alarm
   status: boolean;
 }
