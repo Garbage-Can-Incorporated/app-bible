25128032e3dd1ed3efbf2d5b55a14dd0232054ee
Israel Oluwole <olaleyeisrael@gmail.com>
2020-04-13 16:07:27 +0100

    unsstable factory of search results returned to renderer process

--- handlers/ipc-search.js
+++ handlers/ipc-search.js
@@ -2,19 +2,40 @@ const {ipcMain} = require('electron');
 const childProcess = require('child_process');
 
 const sp = childProcess.fork('./handlers/search-handler.js');
+sp.send(Object.assign({}, {purpose: 'init'}));
+
+// const setup = require('./search-handler');
+// setup.init();
 
 ipcMain.on('search', (e, data) => {
+  let times = 0;
   // speak to forked process;
-  sp.send(data);
+  sp.send(Object.assign({}, {purpose: 'search'}, data));
 
   // receive response
   sp.on('message', (data) => {
     // async results from fuse-subject, would send a few times
-    // console.log({searchResult: data});
-    if (data.status) {
+
+    if (data.status) { // no error
       e.sender.send('search-result', data.result);
+
+      times++;
+      console.log({times, result: data.result.item.chapterNo});
     }
   });
+
+  /* setup.cbSearch(
+      Object.assign({}, {purpose: 'search'}, data),
+      (res) => {
+        if (res.status) {
+          e.sender.send('search-result', res.result);
+
+          times++;
+          console.log({times, result: res.result.item.chapterNo});
+        }
+      }); */
+
+  times = 0;
 });
 
 
--- handlers/search-handler.js
+++ handlers/search-handler.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const {observeOn, distinctUntilChanged} = require('rxjs/operators');
+const {asyncScheduler} = require('rxjs');
+
 const Fuse = require('fuse.js');
 
 const options = {
@@ -12,14 +15,13 @@ const options = {
   includeScore: true,
   includeMatches: true,
   threshold: 0.0,
-  // location: 0,
-  // distance: 100,
   maxPatternLength: 100,
   minMatchCharLength: 5,
   keys: ['verses'],
 };
 let resource;
 let fuse;
+let subscription;
 
 const getResource = (cb) => {
   if (resource !== undefined) {
@@ -40,26 +42,91 @@ const getResource = (cb) => {
   }
 };
 
-getResource((data) => {
-  fuse = new Fuse(data, options);
 
-  process.on('message', (_data) => {
+const setup = Object.freeze({
+  init: () => {
+    getResource((data) => {
+      fuse = new Fuse(data, options);
+    });
+  },
+  search: (_data, cb) => {
+    if (subscription !== undefined) {
+      subscription.unsubscribe();
+    }
+
+    subscription = fuse
+        .subject
+        .pipe(
+            observeOn(asyncScheduler),
+            distinctUntilChanged()
+        )
+        .subscribe(
+            (result) => {
+              if (result) {
+                process.send({result, status: true});
+              } else {
+                // nothing was found
+                process.send({result: null, status: true});
+              }
+            },
+            (error) => {
+              console.log({error});
+              process.send({result: null, status: false});
+            },
+            () => {
+              console.log('complete indeed!');
+              // fuse.subject.unsubscribe();
+            }
+        );
+
     fuse.search(_data.query);
+  },
+  cbSearch: (_data, cb) => {
+    /* if (subscription !== undefined) {
+      subscription.unsubscribe();
+    } */
 
-    fuse.subscribe(
-        (result) => {
-          if (result) {
-            process.send({result, status: true});
-          } else {
-          // nothing was found
-            process.send({result: null, status: true});
-          }
-        },
-        (error) => {
-          console.log({error});
-          process.send({result: null, status: false});
-        }
-    );
-  });
+    /* subscription =  */fuse
+        .subject
+        .pipe(
+            // observeOn(asyncScheduler),
+            // distinctUntilChanged()
+        )
+        .subscribe(
+            (result) => {
+              if (result) {
+                // process.send({result, status: true});
+                cb({result, status: true});
+              } else {
+                // nothing was found
+                // process.send({result: null, status: true});
+                cb({result, status: true});
+              }
+            },
+            (error) => {
+              console.log({error});
+              // process.send({result: null, status: false});
+              cb({result: null, status: false});
+            },
+            () => {
+              console.log('complete indeed!');
+              fuse.subject.unsubscribe();
+            }
+        );
+
+    fuse.search(_data.query);
+  },
 });
 
+process.on('message', (_data) => {
+  if (_data.purpose === 'init') {
+    setup.init();
+    return;
+  }
+
+  if (_data.purpose === 'search') {
+    setup.search(_data);
+  }
+});
+
+module.exports = setup;
