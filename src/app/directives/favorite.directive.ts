741577c213a971ed2f0f14987652bde8c390a22b
Israel Oluwole <olaleyeisrael@gmail.com>
2020-05-02 12:29:28 +0100

    removed electron properties

--- src/app/directives/favorite.directive.ts
+++ src/app/directives/favorite.directive.ts
@@ -1,40 +1,22 @@
-import { Directive, HostListener, OnInit, Input, ElementRef, OnChanges, OnDestroy } from '@angular/core';
+import { Directive, HostListener, OnInit, Input, ElementRef, OnChanges } from '@angular/core';
 
-import { ElectronService } from 'ngx-electron';
 import { SnackbarService } from '../services/snackbar.service';
-import { DbIpcService } from '../services/db-ipc.service';
 
 import { IScriptures } from '../interfaces/i-scriptures';
-import { IpcMainResponse } from '../interfaces/ipc-main-response';
 
 @Directive({
   selector: '[appFavorite]'
 })
-export class FavoriteDirective implements OnInit, OnDestroy, OnChanges {
+export class FavoriteDirective implements OnInit, OnChanges {
   @Input() scripture: IScriptures;
   @Input() toggleIcon: boolean;
-  private dbStatus: boolean;
-  private tableStatus: boolean;
 
   constructor(
-    private _electron: ElectronService,
     private _snackbar: SnackbarService,
-    private el: ElementRef,
-    private _dbIPC: DbIpcService
+    private el: ElementRef
   ) { }
 
-  ngOnInit() {
-    if (this.isElectronApp) {
-      this._dbIPC
-        .isDBCreated('favorites');
-
-      return;
-    }
-  }
-
-  ngOnDestroy() {
-    // this._dbIPC.subjects.isConnectedSubject.unsubscribe();
-  }
+  ngOnInit() { }
 
   ngOnChanges() {
     if (this.toggleIcon) {
@@ -42,23 +24,12 @@ export class FavoriteDirective implements OnInit, OnDestroy, OnChanges {
     } else {
       this.removeFavIcon(this.el.nativeElement);
     }
-
-    if (this.isElectronApp) {
-      this._dbIPC
-        .isConnected('favorites')
-        .subscribe(
-          (data: {dbInit: boolean, tableInit: boolean}) => {
-            this.dbStatus = data.dbInit;
-            this.tableStatus = data.tableInit;
-          }
-        );
-    }
   }
 
   @HostListener('click', [ '$event' ])
   onClick(e: Event): void {
     if (this.toggleIcon) {
-      // remove from fav db
+      // remove from fav local storage
       this.removeFavVerse(e.target);
     } else {
       this.addToFavVerse(e.target);
@@ -66,63 +37,11 @@ export class FavoriteDirective implements OnInit, OnDestroy, OnChanges {
   }
 
   private removeFavVerse(e: EventTarget): void {
-    if (this.isElectronApp && this.dbStatus && this.tableStatus) {
-      this._electron.ipcRenderer.send(
-        'remove-fav-item',
-        { scripture: this.scripture }
-      );
 
-      this.waitFavRemovalStatus(e);
-
-      return;
-    }
-  }
-
-  private waitFavRemovalStatus(evt: EventTarget): void {
-    this._electron.ipcRenderer.on(
-      'fav-item-removal-status',
-      (e, data: IpcMainResponse) => {
-        if (data.status) {
-          this.addFavIcon(evt);
-        } else {
-          this.removeFavIcon(evt);
-        }
-
-        // remove snackbar
-        this._snackbar
-          .showSnackbar(data.message);
-      }
-    );
   }
 
   private addToFavVerse(e: EventTarget): void {
-    if (this.isElectronApp && this.dbStatus && this.tableStatus) {
-      this._electron.ipcRenderer.send(
-        'add-fav-item',
-        { scripture: this.scripture }
-      );
 
-      this.waitFavAdditionStatus(e);
-
-      return;
-    }
-  }
-
-  private waitFavAdditionStatus(evt: EventTarget): void {
-    this._electron.ipcRenderer.on(
-      'fav-item-addition-status',
-      (e, data: IpcMainResponse) => {
-        if (data.status) {
-          this.addFavIcon(evt);
-        } else {
-          this.removeFavIcon(evt);
-        }
-
-        // remove snackbar
-        this._snackbar
-          .showSnackbar(data.message);
-      }
-    );
   }
 
   private addFavIcon(el: any): void {
@@ -139,9 +58,5 @@ export class FavoriteDirective implements OnInit, OnDestroy, OnChanges {
       el.classList.remove('__red--color');
       return;
     }
-  }
-
-  private get isElectronApp(): boolean {
-    return this._electron.isElectronApp;
   }
 }
--- src/app/directives/twitter-share.directive.ts
+++ src/app/directives/twitter-share.directive.ts
@@ -1,7 +1,5 @@
 import { Directive, HostListener, Input } from '@angular/core';
 
-import { RendererBwService } from '../services/renderer-bw.service';
-
 @Directive({
   selector: '[appTwitterShare]'
 })
@@ -10,20 +8,10 @@ export class TwitterShareDirective {
   @Input() public text: string;
   private hashtags = <string> `ev'ryword`;
 
-  constructor(
-    private _bwService: RendererBwService
-  ) { }
+  constructor() { }
 
   @HostListener('click', [ '$event' ])
   onClick(e: Event): void {
-    this._bwService
-      .createWindow(
-        this.url,
-        {
-          text: this.text, hashtags: this.hashtags, via: `ev'ryword`
-        }
-      )
-      .then((value) => console.log(`[BW] window opened successfully ${value}`))
-      .catch((err) => console.log(`[BW] An error occured ${err}`));
+    // use url directly
   }
 }
