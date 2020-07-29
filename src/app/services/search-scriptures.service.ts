bcc4a778af509028973798ba3d6bb5282d8aed80
Israel Oluwole <olaleyeisrael@gmail.com>
2020-07-29 01:06:12 +0100

    refactor: snackbar and electron env check

--- src/app/services/search-scriptures.service.ts
+++ src/app/services/search-scriptures.service.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
 import { ElectronService } from 'ngx-electron';
 
 import { ISearchResults } from '../interfaces/i-search-results';
+import {SnackbarService} from './snackbar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +14,37 @@ export class SearchScripturesService {
   private searchSubject: Subject<ISearchResults> = new Subject();
 
   constructor(
+    private snackbar: SnackbarService,
     private _electron?: ElectronService
   ) { }
 
   public listenResult(): void {
-    this._electron.ipcRenderer
-      .on(
-        'search-result',
-        (_, data: ISearchResults) => {
-          this.searchSubject.next(data);
-        });
+    if (this._electron.isElectronApp) {
+      this._electron.ipcRenderer
+        .on(
+          'search-result',
+          (_, data: ISearchResults) => {
+            this.searchSubject.next(data);
+          });
+    } else {
+      this.showErrorSnackbar();
+    }
   }
 
   public search(string: string): void {
-    this._electron.ipcRenderer
-      .send('search', {query: string});
+    if (this._electron.isElectronApp) {
+      this._electron.ipcRenderer
+        .send('search', {query: string});
+    } else {
+      this.showErrorSnackbar();
+    }
   }
 
   public get subject(): Subject<ISearchResults> {
     return this.searchSubject;
+  }
+
+  private showErrorSnackbar(): void {
+    this.snackbar.showSnackbar('Not an Electron app');
   }
 }
