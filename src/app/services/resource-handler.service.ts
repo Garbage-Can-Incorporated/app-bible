8d69f5c13a6a2b913ec288aaed35145289d77125
Israel Oluwole Olalekan <olaleyeisrael@gmail.com>
2021-08-02 19:54:22 +0100

    feat: 22+versions inclusion beta

--- src/app/services/resource-handler.service.ts
+++ src/app/services/resource-handler.service.ts
@@ -3,28 +3,39 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 export interface BibleElement {
-  version: string,
-  verses: Array<any>,
-  bookTitle: string,
-  chapterNo: string
-};
+  version: string;
+  verses: Array<any>;
+  bookTitle: string;
+  chapterNo: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourceHandlerService {
-  private url = './assets/resource';
+  private url = 'https://api-ent-bible.appspot.com/v1/api';
+  // private url = 'http://localhost:4445/v1/api';
   private resource: Subject<Array<BibleElement>> = new Subject();
 
   constructor(
     private http: HttpClient
   ) { }
 
+  public getVersions(): Observable<{data: string[]}> {
+    return this.http.get<{data: string[]}>(`${this.url}/versions`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
   public get getURL(): string {
-    let version;
-    return `${this.url}/bible-${version || 'kjv'}.json`;
+    return `${this.url}/all`;
   }
 
   public fetchResource(): Observable<{request: Array<BibleElement>}> {
@@ -32,8 +43,9 @@ export class ResourceHandlerService {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
+      params: {vrsn: window.localStorage.getItem('sv') || 'kjv'},
       observe: 'body',
       responseType: 'json',
-    });
+    }).pipe(shareReplay(1));
   }
 }
